1ca1b8bbb4da4a23c8f3e08e095e6a09cbaa2453
TravisKnop <travisknop@gmail.com>
2015-11-15 16:12:01 -0500

    added admin pages

--- movieapp/admin.py
+++ movieapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Movie, Rater, Rating
+
+
+class MovieAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Movie
+
+admin.site.register(Movie)
+admin.site.register(Rater)
+admin.site.register(Rating)
